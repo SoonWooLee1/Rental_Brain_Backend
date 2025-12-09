b141e6a32d7cd7ecf5079f4967d1eda34665f6b2
SoonWooLee1 <lsw010420@gmail.com>
2025-12-09 11:02:10 +0900

    no message

---
+++ rental-brain/src/main/java/com/devoops/rentalbrain/RentalBrainApplication.java
@@ -0,0 +1,13 @@
+package com.devoops.rentalbrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalBrainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentalBrainApplication.class, args);
+    }
+
+}
---
+++ rental-brain/src/test/java/com/devoops/rentalbrain/RentalBrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devoops.rentalbrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalBrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
